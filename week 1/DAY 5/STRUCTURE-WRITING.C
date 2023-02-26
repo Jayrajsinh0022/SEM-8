bf8cdeb96b9c9d3c247fa5091383e7c6b5b0844e
Jayrajsinh0022 <107838883+Jayrajsinh0022@users.noreply.github.com>
2023-02-26 11:35:39 +0530

    Create STRUCTURE-WRITING.C

---
+++ week 1/DAY 5/STRUCTURE-WRITING.C
@@ -0,0 +1,49 @@
+/*
+Author: JAYRAJ SISODIYA
+Date of Creation: 9th Feb 2023
+Subject: Structure Writing
+*/
+
+#include<stdio.h>
+#include<conio.h>
+struct Student
+{
+  int sid;
+  char name[30];
+}s[2];
+
+void scan()
+{
+  int i;
+  FILE *fp;
+  fp=fopen("student.txt","w");
+  for(i=0;i<2;i++)
+  {
+    printf("\nEnter the sid and name: ");
+    scanf("%d%s",&s[i].sid,s[i].name);
+    fwrite(&s[i],sizeof(s[i]),1,fp);
+  }
+  fclose(fp);
+}
+           
+void display()
+{
+ FILE *fp;
+ clrscr();
+ int i;
+ printf("\nsid\tname");
+ fp=fopen("student.txt","r");
+ for(i=0;i<2;i++)
+ {
+  fread(&s[i],sizeof(s[i]),1,fp);
+  printf("\n%d\t%s",s[i].sid,s[i].sname);
+ }
+ fclose(fp);
+}
+
+void main()
+{
+  //scan();
+  display();
+  getch();
+}
