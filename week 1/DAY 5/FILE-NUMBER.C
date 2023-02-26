409260edfec5a4692e9e9f05bd9ff308cf5326dd
Jayrajsinh0022 <107838883+Jayrajsinh0022@users.noreply.github.com>
2023-02-26 11:32:17 +0530

    Create FILE-NUMBER.C

---
+++ week 1/DAY 5/FILE-NUMBER.C
@@ -0,0 +1,53 @@
+/*
+Author: JAYRAJ SISODIYA
+Date of Creation: 9th Feb 2023
+Subject: To read and write the numbers in file
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  FILE *fp1,*fp2;
+  int num,i;
+  clrscr();
+  
+  fp1=fopen("even.txt","w+");
+  fp2=fopen("odd.txt","w+");
+  for(i=0;i<10;i++)
+  {
+    printf("\nEnter the number");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+      putw(num,fp1);
+    }
+    else
+    {
+      putw(num,fp2);
+    }
+  }
+  
+  printf("\nThe cursor is at %d place",ftell(fp1));
+  printf("\nThe cursor is at %d place",ftell(fp2));
+  fseek(fp1,0L,SEEK_SET); //SEEK_CUR SEEK_END SEEK_SET
+  fseek(fp2,0L,SEEK_SET);
+  printf("\nThe cursor is at %d place",ftell(fp1));
+  printf("\nThe cursor is at %d place",ftell(fp2));
+  getch();
+  clrscr();
+  printf("\nThe even numbers are as follows: ");
+  while(num=getw(fp1))!=EOF)
+  {
+    printf("%d\t",num);
+  }
+  fclose(fp1);
+  printf("\n The odd numbers are as follows: ");
+  while((num=getw(fp2))!=EOF)
+  {
+    printf("%d\t",num);
+  }
+  fclose(fp2);
+  getch();
+}
